2c8b8d4dd947b74e9cc77c3cb3a113de1f58622b
Ianko Djemerenov <iankodj@gmail.com>
2017-11-02 13:31:41 +0200

    Updates

--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { period } from './period';
-import { issues } from './issues';
-import { users } from './users';
-
-export const issuesDashboard = combineReducers({period, issues, users});
--- src/store/index.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from "redux";
-import { issuesDashboard } from '../reducers';
-
-export const issuesStore = createStore(issuesDashboard, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
